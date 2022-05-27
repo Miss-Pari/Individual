9ebcaaf7bb07d507072a5952d40a828cb48223f9
Miss-Pari <praptiajgaonkar@gmail.com>
2022-05-28 01:22:07 +0530

    insert page xml and .java error when I added line 14 following code added after error occured DatabaseReference databaseMinerals;
    databaseMinerals = FirebaseDatabase.getInstance().getReference("minerals");
    
    String id = databaseMinerals.push().getKey();
    Minerals minerals = new Minerals(id, english, konkani, pro);
    databaseMinerals.child(id).setValue(minerals);
    Toast.makeText(this, "mineral added", Toast.LENGHT_S HORT).show();

--- app/src/main/java/com/practical/individual/InsertActivity.java
+++ app/src/main/java/com/practical/individual/InsertActivity.java
@@ -1,4 +1,57 @@
 package com.practical.individual;
 
-public class InsertActivity {
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class InsertActivity extends AppCompatActivity {
+
+    EditText english, konkani, pro;  //pro==pronounce(column id)
+    Button save;
+
+    DatabaseReference databaseMinerals;
+
+    protected void onCreate(Bundle savedInstances) {
+        super.onCreate(savedInstances);
+        setContentView(R.layout.insert_layout);
+
+        databaseMinerals = FirebaseDatabase.getInstance().getReference("minerals");
+        
+        english = (EditText) findViewById(R.id.english);
+        konkani = (EditText) findViewById(R.id.konkani);
+        pro = (EditText) findViewById(R.id.pronounce);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addEntry();
+            }
+        });
+    }
+
+    private void addEntry(){
+        String englishString = english.getText().toString().trim();
+        String konkaniString = konkani.getText().toString().trim();
+        String proString = pro.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(englishString)||!TextUtils.isEmpty( konkaniString)||!TextUtils.isEmpty(proString)){
+
+            String id = databaseMinerals.push().getKey();
+            Minerals minerals = new Minerals(id, english, konkani, pro);
+            databaseMinerals.child(id).setValue(minerals);
+            Toast.makeText(this, "mineral added", Toast.LENGTH_SHORT).show();
+            
+        }else{
+            Toast.makeText(this, "fill out all fields u admin!!!!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 }
