df475fc76b9d45cac2ff3118537048cd37133a8e
Smruthul12 <msmruthul2002@gmail.com>
2022-05-28 03:18:01 +0530

    last commit before i sleep

--- app/src/main/java/com/practical/individual/MainActivity.java
+++ app/src/main/java/com/practical/individual/MainActivity.java
@@ -2,13 +2,31 @@ package com.practical.individual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextInputLayout username,password;
+    Button gobtn,logintoregbtn,forgotpass;
+    TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        forgotpass = findViewById(R.id.forgotpass);
+        title=findViewById(R.id.title);
+        gobtn = findViewById(R.id.gobtn);
+        logintoregbtn = findViewById(R.id.logintoregbtn);
+
+        logintoregbtn.setOnClickListener();
     }
+
+
 }
--- app/src/main/java/com/practical/individual/RegisterActivity.java
+++ app/src/main/java/com/practical/individual/RegisterActivity.java
@@ -1,7 +1,13 @@
 package com.practical.individual;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+    }
 }
